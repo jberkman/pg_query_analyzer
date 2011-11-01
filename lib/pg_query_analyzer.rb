f27d6d9c219b9d81314ab4e5a297d66e3701ecc1
Armand du Plessis <adp@bank.io>
2011-11-01 17:07:53 +0200

    Log implementation

--- lib/pg_query_analyzer.rb
+++ lib/pg_query_analyzer.rb
@@ -1,15 +1,15 @@
 module ActiveRecord
   module ConnectionAdapters
-    class PostgreSQLAdapter < AbstractAdapter
+    class AbstractAdapter
       
       @@analyzer_debug = 0..2
       @@analyzer_warn  = 3..7
 
-      def select_logger time_spent, log
+      def select_logger time_spent, message
         case time_spent
-        when @@analyzer_debug then @logger.debug(log)
-        when @@analyzer_warn  then @logger.warn(log)
-        else @logger.fatal(log)
+        when @@analyzer_debug then @logger.debug(message)
+        when @@analyzer_warn  then @logger.warn(message)
+        else @logger.fatal(message)
         end
       end
 
@@ -19,24 +19,31 @@ module ActiveRecord
       # VERBOSE shows the full plan tree, rather than a summary.
       @@explain_verbose = true # use nil to disable
 
-      private
-
-      alias_method :select_without_analyzer, :select
-
-      def select(sql, name = nil)
+      protected
+      
+      alias_method :log_without_analyzer, :log
+      
+      def log(sql, name = "SQL", binds = [], &block)
         start_time = Time.now
-        query_results = select_without_analyzer(sql, name)
+        query_results = log_without_analyzer(sql, name, binds, &block)
         spent = Time.now - start_time
-
+        debugger
         if @logger and @logger.level <= Logger::INFO
-          select_logger(@spent, @logger.silence do
-           format_log_entry("Analyzing #{name} Execution Time: #{spent}\n\n",
-           "#{select_without_analyzer("explain #{'analyze' if @@explain_analyze} "+
-           "#{'verbose' if @@explain_verbose} #{sql}", name).map(&:values).join("\n ")}\n")
-         end) if sql =~ /^select/i
+          if sql.strip =~ /^select/i
+            explain_query = "explain #{'analyze' if @@explain_analyze} " +
+              "#{'verbose' if @@explain_verbose} #{sql}"
+            message = nil
+            @logger.silence do
+              analyze_results = exec_cache(explain_query, []) 
+              message = "Analyzing #{name} Execution Time: #{spent}\n\n"
+              message << analyze_results.map(&:values).join("\n ")
+            end
+            select_logger(spent, message)
+          end
         end
-      query_results
+        query_results
       end
+      
     end
   end
 end
